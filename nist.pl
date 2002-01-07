76903e3f02baf021697c608faa8e67b83d54030c
Ali Onur Cinar <aocinar@cpan.org>
2002-01-07 12:46:58 +0000

    Import of AOCINAR/nist-2.1.13 from CPAN.
    gitpan-cpan-distribution: nist
    gitpan-cpan-version:      2.1.13
    gitpan-cpan-path:         AOCINAR/nist-2.1.13.tar.gz
    gitpan-cpan-author:       AOCINAR
    gitpan-cpan-maturity:     released

--- nist.pl
+++ nist.pl
@@ -1,15 +1,18 @@
-#!/usr/local/bin/perl
-#        $Id: nist.pl,v 1.12 2000/02/07 07:02:18 j Exp $
+#!/usr/bin/perl
+#        $Id: nist.pl,v 1.13 2000/02/07 07:02:18 j Exp $
 #
 # Nist.pl to keep your system time up to date by using time servers.
-# Copyright (C) 1998, 1999, 2000 Ali Onur Cinar <root@zdo.com>
+# Copyright (C) 1998 - 2002 Ali Onur Cinar <root@zdo.com>
+#
 # Y2K fixes by Frank Denis aka Jedi/Sector One <j@4u.net>
+# BSD-based OS detection added by Will (dirtymac) <dirtymac@datasync.com>
 #
 # Latest version can be downloaded from:
 #
 #   http://www.zdo.com
-#   ftp://hun.ece.drexel.edu/pub/cinar/nist*
-#   ftp://ftp.cpan.org/pub/CPAN/authors/id/A/AO/AOCINAR/nist*
+#   http://briefcase.yahoo.com/cinara  (Downloads folder)
+#   http://www.cpan.org (Check for mirror sites)
+#   ftp://mirror.hiwaay.net/CPAN/authors/id/A/AO/AOCINAR/nist*
 #   ftp://sunsite.unc.edu/pub/Linux/system/admin/timei/nist*
 #
 # This program is free software; you can redistribute it and/or
@@ -31,9 +34,9 @@
 use Socket qw(PF_INET SOCK_STREAM AF_INET);
 use POSIX qw(mktime);
 
-$timeserver = 'time_a.timefreq.bldrdoc.gov';	# time server
+$timeserver = 'time-A.timefreq.bldrdoc.gov';	# time server
 $port = '13';					# time port (default:13)
-$timediff = '+01:00:00';			# time differance
+$timediff = '-06:00:00';			# time differance
 $datepr = '/bin/date';				# full path of date
 
 
@@ -43,8 +46,8 @@ $diff = "$1$diff";
 
 if ($> ne 0)
 {
-	print STDERR "This program should run as root user to be able to update sytem date.\n"; 
-#	exit;
+	print STDERR "This program should run as root user to be able to update sytem date.\n";
+	exit;
 }
 
 if ($timeserver =~ /^(\d+)\.(\d+)\.(\d+)\.(\d+)$/)
@@ -81,9 +84,19 @@ print "Current GMT is $1-$2-$3 $4-$5-$6\n";
 $datsec += $diff;
 &SecToDate($datsec);
 
-printf "Local time is  %02d-%02d-%02d %02d-%02d-%02d\n", $year, $month, $day, $hour, $min, $sec;
+printf ("Local time is  %02d-%02d-%02d %02d-%02d-%02d\n", $year, 
+$month, $day, $hour, $min, $sec);
 print "Updating the system time. ";
-$date_command = sprintf ("$datepr %02d%02d%02d%02d%04d.%02d",$month, $day, $hour,$min, $year, $sec);
+
+$os = `uname`;
+if ($os =~ m/BSD/i) {
+  $date_command = sprintf ("$datepr %04d%02d%02d%02d%02d.%02d",$year, 
+$month, $day, $hour, $min, $sec);
+} else {
+  $date_command = sprintf ("$datepr %02d%02d%02d%02d%04d.%02d",$month, 
+$day, $hour,$min, $year, $sec);
+}
+
 system ($date_command);
 print "Done.\n";
 
@@ -91,14 +104,18 @@ sub DateToSec
 {
 	$year = @_[0];$month = @_[1];$day = @_[2];
 	$hour = @_[3];$min = @_[4];$sec = @_[5];
-	$datsec = POSIX::mktime($sec, $min, $hour, $day - 1, $month - 1, $year + 100);
+	$datsec = POSIX::mktime($sec, $min, $hour, $day - 1, $month - 1, $year 
++ 100);
 }
 
 sub SecToDate
 {
 	$datsec = @_[0];
-	($sec, $min, $hour, $day, $month, $year, $wday, $yday, $isdst) = localtime($datsec);
+	($sec, $min, $hour, $day, $month, $year, $wday, $yday, $isdst) = 
+localtime($datsec);
 	$day++;
 	$month++;
 	$year += 1900;
 }
+
+
